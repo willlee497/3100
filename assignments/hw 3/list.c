0ef0d93ad055de9bf2f13c9bd537378eae876d89
willlee497 <162823988+willlee497@users.noreply.github.com>
2024-09-23 04:58:09 +0000

    Describe your fix

--- assignments/hw 3/list.c
+++ assignments/hw 3/list.c
@@ -6,15 +6,31 @@
 void initList(IntList* list)
 {
   /*
-    TODO: The IntList structure is already allocated in memory. You need to fill the fields in. 
+    TODO: The IntList structure is already allocated in memory. You need to fill the fields in. In the initList function, you are passed a pointer to an IntList. You just need to set the head and tail fields
+to NULL, and the len field to 0. For the freeList, you need to iterate through the list, and call free on each
+node( head and tail to null and lenfield 0)
    */
+  list->head = NULL;
+  list->tail = NULL;
+  list->len = 0;
 }
 
 void freeList(IntList* list)
 {
-  /*
+  /* (call free on each node)
     TODO: You need to free each node in the list. However, the actual list itself doesn't need to be freed here.
    */
+  ILNode* current = list->head;
+  ILNode* next; 
+
+  while(current!= NULL){
+    next = current->next;
+    free(current);
+    current = next;
+  }
+  list->head = NULL;
+  list->tail = NULL;
+  list->len = 0;
 }
 
 void pushBackList(IntList* list, int number)
@@ -22,14 +38,48 @@ void pushBackList(IntList* list, int number)
   /*
     TODO: Add a node containing the number to the end of the list
    */
+  
+  ILNode* new = (ILNode*)malloc(sizeof(ILNode));
+  new->number = number;
+  new->next = NULL;
+  if (list->tail == NULL){
+    list->head = new;
+    list->tail = new;
+
+  }else{
+    list->tail->next = new;
+    list->tail = new;
+  }
+  list->len++;
 }
 
 int popBackList(IntList* list)
 {
   assert(list->head && list->tail && list->len > 0);
+    ILNode* tempo = list->tail;
+    int val = tempo->number;
+  if (list->len == 1){
+    free(tempo);
+    list->head = list->tail = NULL;
+  }else{
+    ILNode* current = list->head;
+    while(current->next != list->tail){
+      current = current->next;
+      
+    }
+    current->next = NULL;
+    list->tail = current;
+    free(tempo);
+    
+
+  }
+  list->len--;
+  return val;
+
     /*
     TODO: Remove the tail node of the list, and return the integer contained in it.
   */
+
 }
 
 void pushFrontList(IntList* list, int number)
@@ -37,11 +87,33 @@ void pushFrontList(IntList* list, int number)
   /*
     TODO: Add a node containing the number to the start of the list 
   */
+  ILNode* new = (ILNode*)malloc(sizeof(ILNode));
+  new->number = number;
+  if (list->len == 0){
+    list->head = new;
+    list->tail = new;
+    new->next = NULL;
+  }else{
+    new->next = list->head;
+    list->head = new;
+  }
+  list->len++;
 }
 
 int popFrontList(IntList* list)
 {
   assert(list->head && list->tail && list->len > 0);
+  ILNode* temp = list->head;
+  int val = temp->number;
+  list->head = list->head->next;
+  if (list->len = 0){
+    list->tail == NULL;
+  }
+  free(temp);
+  list->len--;
+  return val;
+  
+  
   /*
     TODO: Remove the head node of the list, and return the integer contained in it. 
   */
@@ -51,6 +123,27 @@ void sortedInsert(IntList* sortedList, ILNode* nodeToInsert){
   /*
     TODO: You're given a linked list where the nodes are sorted (the smallest number is at the head, the largest at the tail). Iterate through the list until you find where to insert nodeToInsert, so as to keep the list sorted, and insert it. 
    */
+  if (sortedList->len == 0 || nodeToInsert->number <= sortedList->head->number){
+    nodeToInsert->next = sortedList->head;
+    sortedList->head = nodeToInsert; 
+    if (sortedList->len == 0){
+      sortedList->tail = nodeToInsert;
+    }
+
+  }else{
+    ILNode* current = sortedList->head;
+    while(current->next != NULL && current->next->number < nodeToInsert->number){
+      current = current->next;
+    }
+    nodeToInsert->next = current->next;
+    current->next = nodeToInsert;
+    if(nodeToInsert->next == NULL){
+      sortedList->tail = nodeToInsert;
+  }
+
+  }
+  sortedList->len++;
+
 }
 
 void sortList(IntList* list)
--- assignments/hw 3/vector.c
+++ assignments/hw 3/vector.c
@@ -7,13 +7,20 @@
 void initVector(IntVector* vec, int cap)
 {
   /*
-    TODO: The IntVector structure is already allocated in memory. You need to fill in the fields of it. For this, you'll need to allocate an array for cap integers.
+  list->len = 0;TODO: The IntVector structure is already allocated in memory. You need to fill in the fields of it. For this, you'll need to allocate an array for cap integers.
    */
+  vec->numbers = (IntVector*)malloc(sizeof(IntVector*) * cap)
+  vec->size = 0;
+  vec->capacity = cap;
+
 }
 
 void freeVector(IntVector* vec)
 {
-  /*
+  free(vec->numbers);
+
+  /*  ILNode* current = list->head;
+  ILNode* next; 
     TODO: Free the integer array that the IntVector contains. 
   */
 }
@@ -24,6 +31,7 @@ void expandVector(IntVector* vec)
   /*
     TODO: Use the realloc function to double the capacity of the vector. This is only one line. 
   */
+  vec->numbers = realloc(vec->numbers, sizeof(int)*vec->capacity);
 }
 
 void pushBackVector(IntVector* vec, int number)
@@ -31,21 +39,38 @@ void pushBackVector(IntVector* vec, int number)
   /*
     TODO: First, check if the vector size is equal to capacity. If it is, then expand it. Then, make the last element of the vector be number; don't forget to increase the size field.
    */
-}
+  if (vec->size == vec->capacity){
+    expandVector(vec);
+  } 
+  vec->size++;
+  vec->numbers[vec->size - 1] = number;
 
+}
 int popBackVector(IntVector* vec)
 {
   assert(vec->size > 0); /* asserts that the vector contains at least 1 element */
   /* 
     TODO: Remove and return the last element. Decrease the vector size by 1. 
    */
+  int temp = vec->numbers[vec->size -1];
+  vec->size--;
+  return temp;
 }
 
 void pushFrontVector(IntVector* vec, int number)
 {
+  if (vec->size == vec->capacity){
+    expandVector(vec);
+  }
+
+  for (int i = vec->size; i<= 0; i--){
+    vec->numbers[i] = number[i-1];
+  }
+  vec->numbers[0] = number;
   /*
     TODO: First, check if the vector size is equal to the capacity. If it is, then expand it. Then, insert an element into the front of the vector. Don't forget to increment the size. 
    */
+  vec->size++;
 }
 
 int popFrontVector(IntVector* vec)
@@ -54,6 +79,12 @@ int popFrontVector(IntVector* vec)
   /*
     TODO: Remove and return the number at the head of the vector. Be sure to shift remaining elements over to the left (toward the start of the vector) by one place. 
    */
+  int tempo = vec->numbers[0];
+  for (int j = 1; j<=vec->size ;j++){
+    vec->numbers[i-1] = vec->numbers[i];
+  }
+  vec->size--;
+  return tempo;
 }
 
 void swap(IntVector* vec, size_t indexOne, size_t indexTwo)
@@ -68,8 +99,18 @@ void swap(IntVector* vec, size_t indexOne, size_t indexTwo)
 void sortVector(IntVector* vec)
 {
   /*
-    TODO: Implement Insertion Sort to sort the vector. The swap function will be useful here. 
+    TODO: Impement Insertion Sorlt to sort the vector. The swap function will be useful here. 
    */
+  for(int k = 1; k <=vec->size; k++){
+    int key = vec->numbers[k];
+    int y = k - 1
+    while(y>=0 && vec->numbers[y]> key){
+      vec->numbers[y+1] = vec->numbers[y];
+      y--;
+
+    }
+    vec->numbers[y+1] = key;
+  }
 }
 
 void printVector(IntVector* vec)
