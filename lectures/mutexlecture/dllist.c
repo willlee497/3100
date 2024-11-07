f55be3b0ecfa66956641fa3e4df473d2e4d8f3b8
willlee497 <162823988+willlee497@users.noreply.github.com>
2024-11-07 16:59:42 +0000

    lecture mutex

--- lectures/mutexlecture/dllist.c
+++ lectures/mutexlecture/dllist.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 void initList(DLList* list) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    list->head = list->tail = NULL;
-   pthread_mutex_unlock(&list_>mtx);
+   pthread_mutex_unlock(&list->mtx);
 }
 
 void clearList(DLList* list) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    DLNode* cur = list->head;
    while (cur) {
       DLNode* next = cur->next;
@@ -16,17 +16,17 @@ void clearList(DLList* list) {
       cur = next;
    }
    list->head = list->tail = NULL;
-   pthread_mutex_unlock(&list_>mtx);
+   pthread_mutex_unlock(&list->mtx);
 }
 
 void destroyList(DLList* list) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    clearList(list);
-   pthread_mutex_unlock(&list_>mtx);
+   pthread_mutex_unlock(&list->mtx);
 }
 
 void insertFrontList(DLList* list,int v) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    DLNode* nn = malloc(sizeof(DLNode));
    nn->value = v;
    nn->next = nn->prv = NULL;
@@ -37,11 +37,11 @@ void insertFrontList(DLList* list,int v) {
       list->head->prv = nn;
       list->head = nn;
    }
-   pthread_mutex_unlock(&list_>mtx);
+   pthread_mutex_unlock(&list->mtx);
 }
 
 void insertBackList(DLList* list,int v) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    DLNode* nn = malloc(sizeof(DLNode));
    nn->value = v;
    nn->next = nn->prv = NULL;
@@ -52,40 +52,66 @@ void insertBackList(DLList* list,int v) {
       nn->prv = list->tail;
       list->tail = nn;
    }
-   pthread_mutex_unlock(&list_>mtx);
-}
-
-void insertSortedList(DLList* list,int v) {
-   pthread_mutex_lock(&list_>mtx);
-   DLNode* cur = list->head;
-   while (cur && cur->value <= v)
-      cur = cur->next;
-   if (cur) {
-      // We found the spot: before `cur`
-      DLNode* prev = cur->prv;
-      DLNode* nxt  = cur;
-      DLNode* nn = malloc(sizeof(DLNode));
-      nn->value = v;
-      nn->prv = prev;
-      nn->next = cur;
-      if (prev) prev->next = nn;
-      else list->head = nn;
-      nxt->prv = nn;
-   } else {
-      DLNode* nn = malloc(sizeof(DLNode));
-      nn->value = v;
-      nn->next = NULL;
-      nn->prv = list->tail;
-      if (list->tail)
-         list->tail->next = nn;
-      else list->head = nn;
-      list->tail = nn;
-   }
    pthread_mutex_unlock(&list->mtx);
 }
 
+void insertSortedList(DLList* list,int v) {
+   pthread_mutex_lock(&list->mtx);
+   DLNode* cur = list->head, *prv = NULL;
+   while (cur && cur->value <= v)
+      cur = cur->next;
+   if (cur == NULL) {
+      DLNode* nn = malloc(sizeof(DLNode));
+      pthread_mutex_init(&nn->mtx,NULL);
+      // We found the spot: before `cur`
+      nn->value = v;
+      nn->next = nn->prv = NULL;
+      list->head = list->tail = nn;
+      pthread_mutex_unlock(&list->mtx);
+   } else{
+      pthread_mutex_t* lastLock = &list->mtx;
+      while(cur){
+         pthread_mutex_lock(&cur->mtx);
+         if (cur->value > v) break;
+         pthread_mutex_unlock(lastLock);
+         lastLock = &cur-> mtx;
+         prv = cur;
+         cur = cur->next;
+      }
+      if(cur){
+         DLNode* prev = cur->prv;
+         DLNode* nxt = cur;
+         DLNode* nn = malloc(sizeof(DLNode));
+         pthread_mutex_init(&nn->mtx, NULL);
+         nn->value = v;
+         nn->prev = prev;
+         nn->next = cur;
+         if (prev) prev->next = nn;
+         else list->head = nn;
+                  nxt->prv = nn;                          // nxt is the same as cur. So it's locked. Safe to write.
+         pthread_mutex_unlock(&cur->mtx);        // we are done. unlock current
+         pthread_mutex_unlock(lastLock);         // we are done. unlock the previous (node or list).
+      } else {
+         // Note that CUR == NULL. Therefore only the last node (prv) is locked. (lastLock)
+         DLNode* nn = malloc(sizeof(DLNode));    // safe
+         pthread_mutex_init(&nn->mtx,NULL);      // safe
+         nn->value = v;                          // safe since we are the only one with access to nn
+         nn->next = NULL;                        // see above
+         nn->prv  = prv;                         // point back to former last
+         prv->next = nn;                         // make last point here
+         pthread_mutex_unlock(lastLock);         // done modifying prv
+         pthread_mutex_lock(&list->mtx);
+         list->tail = nn;
+         pthread_mutex_unlock(&list->mtx);
+
+      }
+   }
+}
+  
+      
+
 void insertInListBehind(DLList* list,int key,int v) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    DLNode* cur = list->head;
    while (cur && cur->value != key)      
       cur = cur->next;
@@ -104,7 +130,7 @@ void insertInListBehind(DLList* list,int key,int v) {
 }
 
 void removeFromList(DLList* list,int key) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    DLNode* cur = list->head;
    while (cur && cur->value != key)
       cur = cur->next;
@@ -121,7 +147,7 @@ void removeFromList(DLList* list,int key) {
 }
 
 void printListForward(DLList* list) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    DLNode* cur = list->head;
    while (cur) {
       printf("%d ",cur->value);
@@ -132,7 +158,7 @@ void printListForward(DLList* list) {
 }
 
 void printListBackward(DLList* list) {
-   pthread_mutex_lock(&list_>mtx);
+   pthread_mutex_lock(&list->mtx);
    DLNode* cur = list->tail;
    while (cur) {
       printf("%d ",cur->value);
