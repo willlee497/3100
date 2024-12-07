429194b70a7d52993fbf5946dd6786b82ec4e966
willlee497 <162823988+willlee497@users.noreply.github.com>
2024-12-07 02:53:51 +0000

    s

--- proj./selectionSort.c
+++ proj./selectionSort.c
@@ -18,6 +18,60 @@ void* sortThread(void* args) {
         * If this thread is the last to enter, add the best value to the output array and swap the min element with the first elements of the winning segment. Reset the other fields of the shared structure and update the generation. Broadcast to wake up all threads. Note that if the winning segment has no more values left, the thread should exist and the number of threads expected in the barrier would be one less than before.
         * Finally, unlock the mutex for the shared object.
       */
+      //selection sort using threads. shared data and barriers to sync. sorting process.
+      // each thread needs its generation to share global gen for sync. purposes
+      myGen = shr->gen;
+      //reset thread won field to 0 to indicate that we have not finished sorting 
+      me->won = 0;
+      // segments of the array will be divided for each thread, they gotta cooperate with the others
+      //selection sort n^2 time 
+      //every thread grabs best min val at segment. if we need to compare all threads for best val we need to wait on threads to finish computing. 
+      //if thread finishes work and others didnt, all threads got to wait (barriers) 
+      //this program is reliant on every thread to do the work to get the right answer
+      //each thread iterates over its assigned segment (from to to) then finds min val and index
+      for(int i = me->from; i < me->to; i++){
+        if (me->array[i] <best){
+          best = me->array[i];
+          me->minAt = i;
+        }
+      }
+      //shr->best tracks curr min val
+      //we lock so each thread can compare with global and if its smaller then we update the global with the threads smalelst val
+      pthread_mutex_lock(&shr->mtx);
+      if (shr->winner == NULL || best < shr->best){
+        shr->winner = me;
+        shr->best = best;
+      }
+      shr->reporting++; // indicate thread reaching barrier
+      //reporting tracks how many threads have reached the sync. barrier
+      //shr->needed = # of threads needed that need to be sorting their section
+      //shr->gen (all threads wait for each thread to get min val so we need to get smallest val then 2nd smallest in these iterations)
+      if (shr->reporting == shr->needed){
+        //if all threads reached the thread barrier
+        shr->winner->won = 1;
+        //res array gets best value added and increments index then we perform swap with first unsorted element
+        shr->res[shr->nbIn++] = shr->best;
+        //we mark index and swaps with earliest index (from being earliest index) e.x. if we go from 2-4 from would be 2, then in this case we swap index of min val 
+        //with the val we're at currently smallest index
+        shr->winner->array[shr->winner->minAt] = shr->winner->array[shr->winner->from];
+        shr->winner->array[shr->winner->from] = shr->best;
+        shr->winner->from++; // now we move to next unsorted element
+        if (shr->winner->from == shr->winner->to){ //basically done (from to to are equal now)
+          shr->needed--;
+        } //if we got no more elements we decrement thread participating
+        shr->gen++; //signal a new round of sorting
+        //reset vals
+        shr->best = MAXINT;
+        shr->winner = NULL;
+        shr->reporting = 0;
+        pthread_cond_broadcast(&shr->done);//then we wake up all threas w/ broadcast
+      }
+      else{ //threads that are not the last will have to wait on cond variable
+        pthread_cond_wait(&shr->done, &shr->mtx);
+      }
+      //release mutex and reset thread local best for next iteration
+      pthread_mutex_unlock(&shr->mtx);
+      best = MAXINT;
    }
    return NULL;
 }
